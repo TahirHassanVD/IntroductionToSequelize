e9d0892e3d7f019722c742f56bd12fb1f59a2e50
Tahir Hassan <tahir.hassan@venturedive.com>
2020-04-08 00:54:11 +0500

    add swagger

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const bodyParser = require('body-parser');
 const baseRoutes = require('./src/routes');
 const errorHandler = require('./src/middlewares/error.middleware');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app.use(bodyParser.json());
 app.use('', baseRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(() => errorHandler);
 
